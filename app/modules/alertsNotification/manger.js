0c09b68cebc7f938aba69c1a8a56c5a80cdfc0b7
kajal_KajalBobal <kajal@leewayhertz.com>
2022-03-15 21:13:39 +0530

    code structure fixes

--- app/modules/alertsNotification/manger.js
+++ app/modules/alertsNotification/manger.js
@@ -1,54 +1,60 @@
 import { alertType, amqpConstants, httpConstants } from "../../common/constants";
-import ContractSchema from "../../models/contract";
 import AlertSchema from "../../models/alert";
 import HistorySchema from "../../models/history";
 import AMQPController from "../../../library";
 import Config from "../../../config"
 import Utils from "../../utils";
 import { executeHTTPRequest } from "../../service/http-service";
+import XdcService from "../../service/xdcService";
 import EmailTemplate from '../../../views/emailTemplate';
 
 export default class Manger {
 
     getTransactions = async (transactions) => {
         try {
-            let addresses = []
-            //TODO: transactions.map and then use Set for array duplications
-            for (let index = 0; index < transactions.length; index++) {
-                let cIndex = addresses.findIndex((address) => {
-                    if (transactions[index].contractAddress === address)
-                        return true;
-                })
-                if (cIndex === -1)
-                    addresses.push(transactions[index].contractAddress)
+            Utils.lhtLog("getTransactions", "getTransactions:started Total transactions", { transactionsCount: transactions.length }, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+            let addresses = transactions.map(({ contractAddress }) => contractAddress)
+            addresses = this.getDistinctDatafromArray(addresses)
+
+            let contracts = await XdcService.getSCMSystemContracts(addresses)
+            if (!contracts || !contracts.length) {
+                Utils.lhtLog("getTransactions", "No Contracts for given contract addresses", {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+                return;
             }
-            let contracts = await ContractSchema.getContracts({ "address": { $in: addresses }, isDeleted: false });
-            //TODO: Find Tags and contractAddress from above data
-            let alerts = await AlertSchema.findData({ isDeleted: false, status: true });
-            if (!alerts || !alerts.length) return;
+            Utils.lhtLog("getTransactions", "Get contracts", { contracts }, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+
+            let contractTags = [];
+            contracts.map(({ tags }) => { if (tags.length > 0) contractTags = [...contractTags, ...tags] })
+            Utils.lhtLog("getTransactions", "Get Tag ids", { contractTags }, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+            contractTags = Array.from(new Set(contractTags.map(a => a._id)))
+            Utils.lhtLog("getTransactions", "Get Tag ids", { contractTags }, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+
+            let alerts = await AlertSchema.findData({
+                isDeleted: false, status: true,
+                $or: [{ "target.value": { $in: addresses } }, { "target.value": { $in: contractTags } }]
+            });
+            if (!alerts || !alerts.length) {
+                Utils.lhtLog("getTransactions", "No alerts for given contract addresses", {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+                return;
+            }
+            Utils.lhtLog("getTransactions", "Alerts found for given contracts", { alertsCount: alerts.length }, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
 
             for (let alertIndex = 0; alertIndex < transactions.length; alertIndex++) {
                 let ifAlert = alerts.filter((item) => { if (item.target.value === transactions[alertIndex].contractAddress) return true; })
                 if (ifAlert) {
                     for (let contractIndex = 0; contractIndex < ifAlert.length; contractIndex++) {
-                        await this.checkIfNotification(ifAlert[contractIndex], transactions[alertIndex])
+                        Utils.lhtLog("getTransactions", `Alerts for Contract Address ${transactions[alertIndex].contractAddress}`, ifAlert[contractIndex], "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+                        await this.checkForNotification(ifAlert[contractIndex], transactions[alertIndex])
                     }
                 }
-                let contractTags = [];
-                contracts.filter((item) => {
-                    if (item.address === transactions[alertIndex].contractAddress) {
-                        contractTags = [...contractTags, ...item.tags]
-                    }
-                    return true;
-                })
-                let mySet = new Set(contractTags)
-                contractTags = Array.from(mySet)
-                //TODO: Refactor Tags implementation with _id
                 if (contractTags && contractTags.length) {
                     for (let tagIndex = 0; tagIndex < contractTags.length; tagIndex++) {
                         let tagAlert = alerts.filter((item) => { if (item.target.value === contractTags[tagIndex]) return true; })
                         if (tagAlert) {
-                            for (let contractIndex = 0; contractIndex < tagAlert.length; contractIndex++) { await this.checkIfNotification(tagAlert[contractIndex], transactions[alertIndex]) }
+                            for (let contractIndex = 0; contractIndex < tagAlert.length; contractIndex++) {
+                                Utils.lhtLog("getTransactions", `Alerts for Tag ${contractTags[tagIndex]}`, tagAlert[contractIndex], "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+                                await this.checkForNotification(tagAlert[contractIndex], transactions[alertIndex])
+                            }
 
                         }
                     }
@@ -57,20 +63,22 @@ export default class Manger {
 
         }
         catch (error) {
+            Utils.lhtLog("getTransactions", `Error`, error, "kajal", httpConstants.LOG_LEVEL_TYPE.ERROR)
             throw error;
         }
     }
-    checkIfNotification = async (alert, transaction) => {
+    checkForNotification = async (alert, transaction) => {
+        Utils.lhtLog("checkForNotification", `checkForNotification:started`, { alert, transaction }, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
         switch (alert.type) {
             case alertType.ALERT_TYPE.SUCCESSFULL_TRANSACTIONS.type:
                 if (transaction.status === true) {
-                    Utils.lhtLog("checkIfNotification", alertType.ALERT_TYPE.SUCCESSFULL_TRANSACTIONS.type, {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+                    Utils.lhtLog("checkForNotification", `Alert found for ${alertType.ALERT_TYPE.SUCCESSFULL_TRANSACTIONS.type} in ${transaction.contractAddress}`, {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
                     await sendDataToQueue(transaction, alert)
                 }
                 break;
             case alertType.ALERT_TYPE.FAILED_TRANSACTIONS.type:
                 if (transaction.status === false) {
-                    Utils.lhtLog("checkIfNotification", alertType.ALERT_TYPE.FAILED_TRANSACTIONS.type, {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+                    Utils.lhtLog("checkForNotification", `Alert found for ${alertType.ALERT_TYPE.FAILED_TRANSACTIONS.type} in ${transaction.contractAddress}`, {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
                     await sendDataToQueue(transaction, alert)
                 }
                 break;
@@ -79,7 +87,7 @@ export default class Manger {
                 break;
             case alertType.ALERT_TYPE.TRANSACTION_VALUE.type:
                 if (transaction.value === alert.target.threshold) {
-                    Utils.lhtLog("checkIfNotification", alertType.ALERT_TYPE.TRANSACTION_VALUE.type, {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+                    Utils.lhtLog("checkForNotification", `Alert found for ${alertType.ALERT_TYPE.TRANSACTION_VALUE.type} in ${transaction.contractAddress}`, {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
                     await sendDataToQueue(transaction, alert)
                 }
                 break;
@@ -98,36 +106,41 @@ export default class Manger {
         }
     }
 
+    getDistinctDatafromArray = (array) => {
+        array = new Set(array);
+        return Array.from(array)
+    }
+
 }
 
 const sendDataToQueue = async (transaction, alert) => {
+    Utils.lhtLog("sendDataToQueue", `sendDataToQueue:started`, {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
     if (alert && alert.destinations && alert.destinations.length) {
         let typeName = getTypeName(alert);
         let historyRes = getDataObject(transaction, alert, "", "HISTORY", typeName)
         let historyObject = new HistorySchema(historyRes);
         historyObject["historyId"] = historyObject._id;
         await historyObject.saveData();
+        Utils.lhtLog("sendDataToQueue", `History Object Saved`, historyObject, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+
         let dest = alert.destinations;
-        Utils.lhtLog("sendDataToQueue", "get Destinations", dest, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
+        Utils.lhtLog("sendDataToQueue", `Destinations for alert ${alert._id}`, dest, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
 
         for (let index = 0; index < dest.length; index++) {
             if (dest[index].type === alertType.DESTINATION_TYPE.EMAIL.type) {
                 let mailNotificationRes = getMailNotificationResponse(transaction, alert, dest[index], "MAIL", typeName);
                 Utils.lhtLog("sendDataToQueue", "mailNotificationRes", mailNotificationRes, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO);
                 await AMQPController.insertInQueue(Config.NOTIFICATION_EXCHANGE, Config.NOTIFICATION_QUEUE, "", "", "", "", "", amqpConstants.exchangeType.FANOUT, amqpConstants.queueType.PUBLISHER_SUBSCRIBER_QUEUE, JSON.stringify(mailNotificationRes));
-                Utils.lhtLog("sendDataToQueue", "sendDataToQueue:notification email", {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
             }
             else if (dest[index].type === alertType.DESTINATION_TYPE.SLACK.type) {
                 let slackNotificationRes = getDataObject(transaction, alert, dest[index], "SLACK", typeName)
                 Utils.lhtLog("sendDataToQueue", "slackNotificationRes", slackNotificationRes, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
                 await AMQPController.insertInQueue(Config.NOTIFICATION_EXCHANGE, Config.NOTIFICATION_QUEUE, "", "", "", "", "", amqpConstants.exchangeType.FANOUT, amqpConstants.queueType.PUBLISHER_SUBSCRIBER_QUEUE, JSON.stringify(slackNotificationRes));
-                Utils.lhtLog("sendDataToQueue", "sendDataToQueue:notification slack", {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
             }
             else if (dest[index].type === alertType.DESTINATION_TYPE.WEBHOOK.type) {
                 let slackNotificationRes = getDataObject(transaction, alert, dest[index], "SLACK", typeName)
                 Utils.lhtLog("sendDataToQueue", "webhookNotificationRes", slackNotificationRes, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
                 await executeHTTPRequest(httpConstants.METHOD_TYPE.POST, dest[index].url, slackNotificationRes, {})
-                Utils.lhtLog("sendDataToQueue", "sendDataToQueue:notification webhook", {}, "kajal", httpConstants.LOG_LEVEL_TYPE.INFO)
             }
         }
     }
@@ -143,7 +156,7 @@ const getMailNotificationResponse = (transaction, alert, destination, type, type
         "postedBy": 'XDC SCM',
         "payload": { timestamp: transaction.timestamp, txHash: transaction.hash, contractAddress: transaction.contractAddress, network: transaction.network, userId: alert.userId, typeName: typeName },
         "type": "email",
-        "sentFromEmail": "XFLW@xinfin.org",
+        "sentFromEmail": Config.SENT_FROM_EMAIL,
         "sentFromName": destination.label,
         "addedOn": Date.now(),
     }
