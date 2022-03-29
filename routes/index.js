cd76a779130087e522f2e55cc7e8f04d4e1f3b77
kajal_KajalBobal <kajal@leewayhertz.com>
2022-03-29 18:36:04 +0530

    authentication of apis

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,14 @@ import * as ValidationManger from "../middleware/validation";
 import AlertModule from "../app/modules/alerts";
 import DestinationModule from "../app/modules/destination";
 import HistoryModule from "../app/modules/history";
-import {stringConstants} from "../app/common/constants";
+import { stringConstants } from "../app/common/constants";
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../config/swagger.json';
+import { authenticate } from "../middleware/authentication";
 
 module.exports = (app) => {
     app.get('/', (req, res) => res.send(stringConstants.SERVICE_STATUS_HTML));
-    
+
     /**
      * create swagger UI
      * **/
@@ -20,22 +21,22 @@ module.exports = (app) => {
     /**
      * Alert definition
      */
-    app.post("/alert",ValidationManger.validateAddAlert, new AlertModule().addAlert);
-    app.post("/alert-list", new AlertModule().getAlertList);
-    app.get("/alert/:alertId", new AlertModule().getAlert);
-    app.delete("/alert/:alertId", new AlertModule().deleteAlert);
-    app.put("/alert/:alertId", new AlertModule().updateAlert);
+    app.post("/alert", authenticate, ValidationManger.validateAddAlert, new AlertModule().addAlert);
+    app.post("/alert-list", authenticate, new AlertModule().getAlertList);
+    app.get("/alert/:alertId", authenticate, new AlertModule().getAlert);
+    app.delete("/alert/:alertId", authenticate, new AlertModule().deleteAlert);
+    app.put("/alert/:alertId", authenticate, new AlertModule().updateAlert);
 
     /**
      * Destination definition
      */
-    app.post("/destination",ValidationManger.validateAddDestination, new DestinationModule().addDestination);
-    app.post("/destination-list", new DestinationModule().getDestinations);
-    app.delete("/destination/:destinationId", new DestinationModule().deleteDestination);
+    app.post("/destination", authenticate, ValidationManger.validateAddDestination, new DestinationModule().addDestination);
+    app.post("/destination-list", authenticate, new DestinationModule().getDestinations);
+    app.delete("/destination/:destinationId", authenticate, new DestinationModule().deleteDestination);
 
     /**
      * History definition
      */
-    app.post("/history-list", new HistoryModule().getHistoryList);
+    app.post("/history-list", authenticate, new HistoryModule().getHistoryList);
 
 };
