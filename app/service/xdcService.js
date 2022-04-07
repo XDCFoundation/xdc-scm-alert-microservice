f152ed07284900515b912c35fc185c7a98522f12
kajal_KajalBobal <kajal@leewayhertz.com>
2022-04-07 11:02:10 +0530

    bug fix

--- app/service/xdcService.js
+++ app/service/xdcService.js
@@ -5,7 +5,9 @@ import Config from "../../config"
 export default class XdcService {
 
     static async getSCMSystemContracts(contractArray) {
-        const systemContractsResponse = await HTTPService.executeHTTPRequest(httpConstants.METHOD_TYPE.POST, Config.CONTRACT_SERVICE_BASE_URL + "alert-contracts", {contracts: contractArray},{});
+        const systemContractsResponse = await HTTPService.executeHTTPRequest(httpConstants.METHOD_TYPE.POST, Config.CONTRACT_SERVICE_BASE_URL + "alert-contracts", {contracts: contractArray},{
+            "x-api-key": Config.X_API_KEY
+          });
         return !systemContractsResponse || !systemContractsResponse.success ? null : systemContractsResponse.responseData;
     }
 }
