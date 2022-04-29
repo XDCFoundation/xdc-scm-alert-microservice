e2e1968cce64f6fd30be5959ed34a05cc8c5a774
Sourabh_SourabhArora <sourabh@leewayhertz.com>
2022-04-29 16:54:33 +0530

    new email, slack template updated

--- views/verifyEmailTemplate.js
+++ views/verifyEmailTemplate.js
@@ -1,11 +1,10 @@
 import Config from "../config";
-import moment from "moment" ;
+import moment from "moment";
 
 export default class EmailTemplate {
-
-static createEmail(request , sessionToken) {
-   const url = `${Config.WEB_APP_URL}/alerting?type=Destination&destinationId=${request.destinationId}&sessionToken=${sessionToken}`
-         const emailTemplate = `<head>
+  static createEmail(request, sessionToken) {
+    const url = `${Config.WEB_APP_URL}/alerting?type=Destination&destinationId=${request.destinationId}&sessionToken=${sessionToken}`;
+    const emailTemplate = `<head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,7 +20,7 @@ static createEmail(request , sessionToken) {
           max-height: max-content;
           background-color: white;
           border-radius: 12px 12px 12px 12px !important;
-          margin: 6px auto;
+          margin: 6px auto 12px auto;
         }
         html {
           background-color: #ecf0f7;
@@ -37,58 +36,23 @@ static createEmail(request , sessionToken) {
           background-color: #091f5c;
           padding: 20px;
           border-radius: 12px 12px 0px 0px;
+          display: flex;
         }
         .contentDiv {
           padding: 20px;
           padding-top: 30px;
         }
-        .heading {
-          color: #1f1f1f;
-          font-size: 1.5rem;
-          font-weight: 700;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-            Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-        }
         .content {
-          padding-left: 0px;
-          padding-bottom: 10px;
-          color: #1f1f1f;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-            Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-          font-weight: bold;
-          font-size: 0.875rem;
-        }
-        .values {
-          padding-left: 10px;
-          padding-bottom: 10px;
-          color: #1f1f1f;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-            Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-          font-weight: 400;
-          font-size: 0.875rem;
-        }
-        .alertType {
-          padding-left: 10px;
-          padding-bottom: 10px;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-            Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-          font-weight: 400;
-          font-size: 0.875rem;
-          color : #416BE0
-        }
-        .flexDiv {
-          display: flex;
+          font-weight: 600;
         }
         .container {
-          padding: 20px 0px 0px 0px;
-          color: #1F1F1F;
+          padding: 20px 30px 30px 30px;
+          text-align: center;
         }
-        .lastContainer {
-          padding: 20px 0px 0px 0px;
-        }
+      
         .button {
           background-color: #416be0;
-          margin: 20px auto;
+          margin: 20px 0px;
           border-radius: 4px;
           outline:none;
           border:none;
@@ -100,10 +64,25 @@ static createEmail(request , sessionToken) {
             Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
           font-weight: 600;
           cursor: pointer;
-          width:92%;
-
+          width:100%;
+          max-width: 225px;
+          margin-left: 28.5%;
+        }
+        .headerImg{
+          padding-left: 38.5%;
         }
         .buttonAnchor {
+            color: #416BE0 !important;
+            padding: 5px 0px;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+              Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+            font-weight: 600;
+            cursor: pointer;
+            width:100%;
+            text-decoration:none;
+  
+          }
+          .buttonXmartly {
             color: white !important;
             padding: 5px 0px;
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
@@ -114,29 +93,104 @@ static createEmail(request , sessionToken) {
             text-decoration:none;
   
           }
+          .mainSpan{
+            font-size: 16px;
+            color: #1F1F1F;
+          }
+          
+          .body{
+            background-color: #ECF0F7;
+            padding-top: 9.5%;
+          }
+          .contentMiddle{
+            display: flex;
+            width: 40%;
+            margin-left: 30.5%;
+            color: #7F8691;
+            font-size: 12px;
+            text-align: center;
+          }
+          .footerDiv {
+            display: flex;
+            width: 40%;
+            margin-left: 39.5%;
+            color: #BAC1CC;
+            font-size: 12px;
+            margin-top: 9%;
+            margin-bottom: 2%;
+          }
+
+          .heading{
+            display: flex;
+            width: 40%;
+            margin-left: 35.5%;
+            color: #1F1F1F;
+            font-size: 24px;
+            margin-bottom: 2%;
+            font-weight: 700;
+          }
+
+
         @media (min-width: 300px) and (max-width: 767px){
           .mainDiv{
             min-width: 320px;
             min-height: 320px;
           }
+          .content{
+            font-size: 16px;
+            font-weight: 700;
+          }
+          .contentMiddle{
+            width: 70%;
+            margin-left: 17.5%;
+          }
+          .footerDiv{
+            margin-left: 15.5%;
+            width: 70%;
+          }
+          .button{
+            margin-left: 10.5%;
+          }
+          .headerImg{
+            padding-left: 33.5%;
+          }
         }
+
+        @media (min-width: 768px) and (max-width: 1024px) {
+          .footerDiv{
+            margin-left: 34.5%;
+          }
+        }
+
+        
       </style>
       </head>
-      <body style = "background-color: #f8f8f8">
+      <body class="body">
         <div class="mainDiv">
           <div class="header">
-            <img src="https://xdc-scm-dev.s3.amazonaws.com/Logo.png" alt="" />
+            <img src="https://xdc-scm-dev.s3.amazonaws.com/Logo.png" alt="" class="headerImg" />
           </div>
           <div class="contentDiv">
-            <div class="heading">Verify Email</div>
-            <div class="container">
-             Verify your Email below to get alerts
-              </div>
-            </div>
-           <div class="button"> <a class="buttonAnchor" href=${url}>Yes I confirm this is my email</a> </div>
+          <div class="heading">
+          Verify Email
+          </div>
+          <div class="container">
+         <span class="mainSpan"> Click on the button below to verify your email and start getting notification for the alerts set in Xmartly.</span>
+         </div>
+           <div class="button"> <a class="buttonXmartly" href=${url}>Confirm Email</a> 
+           </div>
           </div>
         </div>
-      </body>`
-        return emailTemplate
-    }
+        <div class="contentMiddle">
+        <span>
+        You are getting this notification because you have set this alert. To manage notifications, <a class="buttonAnchor" href=${Config.WEB_APP_URL}> log in </a> to you account on Xmartly.
+        </span>
+        </div>
+        <div class="footerDiv">
+        Xmartly© Copyright 2022. All rights reserved.
+        </div>
+        <div>
+      </body>`;
+    return emailTemplate;
+  }
 }
